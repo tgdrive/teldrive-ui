551f3d04dcf801c50f6537a0dec0dd7031f8544d
José Avila <aviladiazjose09@gmail.com>
2023-08-20 06:56:00 -0500

    Implements query invalidation for nested paths on upload and delete files

--- src/ui/components/DeleteDialog.tsx
+++ src/ui/components/DeleteDialog.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useCallback } from "react"
+import React, { Dispatch, SetStateAction, useCallback, useMemo } from "react"
 import { ModalState, QueryParams } from "@/ui/types"
 import Button from "@mui/material/Button"
 import Dialog from "@mui/material/Dialog"
@@ -8,6 +8,8 @@ import DialogContentText from "@mui/material/DialogContentText"
 import DialogTitle from "@mui/material/DialogTitle"
 
 import { useDeleteFile } from "@/ui/hooks/queryhooks"
+import { useQueryClient } from "@tanstack/react-query"
+import { getSortOrder } from "../utils/common"
 
 type DeleteDialogProps = {
   modalState: Partial<ModalState>
@@ -21,9 +23,24 @@ export default function DeleteDialog({
 }: DeleteDialogProps) {
   const { mutation: deleteMutation } = useDeleteFile(queryParams)
 
+  const queryClient = useQueryClient();
+  const queryKey = useMemo(() => {
+    const { key, path } = queryParams
+    const sortOrder = getSortOrder()
+    const queryKey = [key, path, sortOrder]
+    return queryKey
+  }, [queryParams])
+
   const handleClose = useCallback((denyDelete = true) => {
     if (!denyDelete) deleteMutation.mutate({ files: modalState.selectedFiles })
     setModalState((prev) => ({ ...prev, open: false }))
+
+    const path = queryKey[1];
+      for (let i = path.length; i > 0; i--) {
+        const partialPath = path.slice(0, i);
+        const updatedQueryKey = ["files", partialPath, queryKey[2]];
+        queryClient.refetchQueries(updatedQueryKey);
+      }
   }, [])
 
   return (
