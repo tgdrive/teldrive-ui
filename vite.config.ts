ab2936c3f24945221f6b800c6e0743b7501ac512
divyam234 <divyam@mnnit.ac.in>
2023-09-09 11:15:47 +0530

    remove service workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,90 +1,11 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig, splitVendorChunkPlugin } from "vite"
-import { VitePWA } from "vite-plugin-pwa"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    splitVendorChunkPlugin(),
-    VitePWA({
-      registerType: "autoUpdate",
-      workbox: {
-        navigateFallbackDenylist: [/^index.html$/,/^\/api/],
-        cleanupOutdatedCaches: true,
-        runtimeCaching: [
-          {
-            urlPattern: "/^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i",
-            handler: "CacheFirst",
-            options: {
-              cacheName: "google-fonts-cache",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          },
-        ],
-        
-      },
-      manifest: {
-        name: "TelDrive",
-        short_name: "Teldrive",
-        start_url: ".",
-        display: "minimal-ui",
-        icons: [
-          {
-            sizes: "114x114",
-            src: "https://github.githubassets.com/apple-touch-icon-114x114.png",
-          },
-          {
-            sizes: "120x120",
-            src: "https://github.githubassets.com/apple-touch-icon-120x120.png",
-          },
-          {
-            sizes: "144x144",
-            src: "https://github.githubassets.com/apple-touch-icon-144x144.png",
-          },
-          {
-            sizes: "152x152",
-            src: "https://github.githubassets.com/apple-touch-icon-152x152.png",
-          },
-          {
-            sizes: "180x180",
-            src: "https://github.githubassets.com/apple-touch-icon-180x180.png",
-          },
-          {
-            sizes: "57x57",
-            src: "https://github.githubassets.com/apple-touch-icon-57x57.png",
-          },
-          {
-            sizes: "60x60",
-            src: "https://github.githubassets.com/apple-touch-icon-60x60.png",
-          },
-          {
-            sizes: "72x72",
-            src: "https://github.githubassets.com/apple-touch-icon-72x72.png",
-          },
-          {
-            sizes: "76x76",
-            src: "https://github.githubassets.com/apple-touch-icon-76x76.png",
-          },
-          {
-            src: "https://github.githubassets.com/app-icon-192.png",
-            type: "image/png",
-            sizes: "192x192",
-          },
-          {
-            src: "https://github.githubassets.com/app-icon-512.png",
-            type: "image/png",
-            sizes: "512x512",
-          },
-        ],
-      },
-    }),
+    splitVendorChunkPlugin()
   ],
 })
