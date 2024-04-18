db24e331779c58597e6f7541f3e44d7288e89b45
divyam234 <47589864+divyam234@users.noreply.github.com>
2024-04-19 04:30:59 +0530

    Update AccountTab component layout and functionality

--- src/components/settings/AccountTab.tsx
+++ src/components/settings/AccountTab.tsx
@@ -105,100 +105,95 @@ export const AccountTab = memo(() => {
   }, [])
 
   return (
-    <div className="flex flex-col gap-3">
-      <div
-        className={clsx(
-          "grid grid-cols-6 gap-8 p-2 w-full overflow-y-auto",
-          scrollbarClasses
-        )}
-      >
-        <div className="col-span-6 xs:col-span-3 flex flex-col justify-around">
-          <div>
-            <p className="text-lg font-medium">Add Bots</p>
-            <p className="text-sm font-normal text-on-surface-variant">
-              Add bots tokens to your account
-            </p>
-          </div>
-          <div className="col-span-6 xs:col-span-3 flex flex-col gap-2">
-            <p className="text-lg font-medium">{`Current Bots: ${data?.bots.length || 0}`}</p>
-            <div className="inline-flex gap-4">
-              <Button
-                title="Copy Tokens to Clipboard"
-                variant="text"
-                className="text-inherit"
-                onPress={copyTokens}
-                isIconOnly
-              >
-                <IcRoundContentCopy />
-              </Button>
-
-              <Button
-                variant="text"
-                title="Remove Bots"
-                className="text-inherit"
-                onPress={removeBots}
-                isLoading={isRemoving}
-                isIconOnly
-              >
-                <IcRoundRemoveCircleOutline />
-              </Button>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-6 xs:col-span-3">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-2"
-          >
-            <Controller
-              name="tokens"
-              control={control}
-              rules={{ required: true, validate: validateBots }}
-              render={({ field, fieldState: { error } }) => (
-                <Textarea
-                  {...field}
-                  disableAutosize
-                  classNames={{
-                    input: "h-32",
-                  }}
-                  placeholder="Enter tokens 1 per line"
-                  autoComplete="off"
-                  errorMessage={error ? error.message : ""}
-                  isInvalid={!!error}
-                />
-              )}
-            />
-            <Button isLoading={isSaving} type="submit" variant="filledTonal">
-              Add Bots
-            </Button>
-          </form>
-        </div>
-        <div className="col-span-6 xs:col-span-3">
-          <p className="text-lg font-medium">Select Channel</p>
+    <div
+      className={clsx(
+        "grid grid-cols-6 gap-8 p-2 w-full overflow-y-auto",
+        scrollbarClasses
+      )}
+    >
+      <div className="col-span-6 xs:col-span-3 flex flex-col justify-around">
+        <div>
+          <p className="text-lg font-medium">Add Bots</p>
           <p className="text-sm font-normal text-on-surface-variant">
-            Select the default telegram channel
+            Add bots tokens to your account
           </p>
         </div>
-        <div className="col-span-6 xs:col-span-3">
-          {data?.channelId && (
-            <Select
-              aria-label="Split File Size"
-              size="lg"
-              isLoading={channelLoading}
-              className="col-span-6 xs:col-span-3"
-              variant="bordered"
-              items={channelData || []}
-              defaultSelectedKeys={[data.channelId.toString()]}
-              onSelectionChange={handleSelectionChange}
+        <div className="col-span-6 xs:col-span-3 flex flex-col gap-2">
+          <p className="text-lg font-medium">{`Current Bots: ${data?.bots.length || 0}`}</p>
+          <div className="inline-flex gap-4">
+            <Button
+              title="Copy Tokens to Clipboard"
+              variant="text"
+              className="text-inherit"
+              onPress={copyTokens}
+              isIconOnly
             >
-              {(channel) => (
-                <SelectItem key={channel.channelId} value={channel.channelId}>
-                  {channel.channelName}
-                </SelectItem>
-              )}
-            </Select>
-          )}
+              <IcRoundContentCopy />
+            </Button>
+
+            <Button
+              variant="text"
+              title="Remove Bots"
+              className="text-inherit"
+              onPress={removeBots}
+              isLoading={isRemoving}
+              isIconOnly
+            >
+              <IcRoundRemoveCircleOutline />
+            </Button>
+          </div>
         </div>
+      </div>
+      <div className="col-span-6 xs:col-span-3">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+          <Controller
+            name="tokens"
+            control={control}
+            rules={{ required: true, validate: validateBots }}
+            render={({ field, fieldState: { error } }) => (
+              <Textarea
+                {...field}
+                disableAutosize
+                classNames={{
+                  input: "h-32",
+                }}
+                placeholder="Enter tokens 1 per line"
+                autoComplete="off"
+                errorMessage={error ? error.message : ""}
+                isInvalid={!!error}
+              />
+            )}
+          />
+          <Button isLoading={isSaving} type="submit" variant="filledTonal">
+            Add Bots
+          </Button>
+        </form>
+      </div>
+      <div className="col-span-6 xs:col-span-3">
+        <p className="text-lg font-medium">Select Channel</p>
+        <p className="text-sm font-normal text-on-surface-variant">
+          Select the default telegram channel
+        </p>
+      </div>
+      <div className="col-span-6 xs:col-span-3">
+        {data?.channelId && (
+          <Select
+            aria-label="Split File Size"
+            size="lg"
+            isLoading={channelLoading}
+            className="col-span-6 xs:col-span-3"
+            variant="bordered"
+            items={channelData || []}
+            defaultSelectedKeys={[data.channelId.toString()]}
+            onSelectionChange={handleSelectionChange}
+          >
+            {(channel) => (
+              <SelectItem key={channel.channelId} value={channel.channelId}>
+                {channel.channelName}
+              </SelectItem>
+            )}
+          </Select>
+        )}
       </div>
     </div>
   )
