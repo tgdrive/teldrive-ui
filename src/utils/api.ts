6cdb8b0eb51618dfc44919c05064a4470b8d2c04
Bhunter <180028024+bhunter234@users.noreply.github.com>
2025-01-26 08:30:41 +0100

    feat: add openapi-react-query dependency and update fetch client implementation

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,12 @@
 import createFetchClient from "openapi-fetch";
-import createClient from "./openapi-query";
+import createClient from "openapi-react-query";
 import type { paths } from "@/lib/api";
 import fetch from "./fetch-throw";
 
 export const fetchClient = createFetchClient<paths>({
   baseUrl: "/api",
   requestInitExt: {
-    signal: AbortSignal.timeout(180 * 1000),
+    signal: AbortSignal.timeout(120 * 1000),
   },
   headers: {
     "Content-Type": "application/json",
